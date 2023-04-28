c2e52aed863a50c9301b314041d9c90a7f9feaed
wumingqiang <1399291367@qq.com>
2023-04-28 16:42:29 +0800

    my-project

---
+++ common/src/main/java/com/jeesite/common/ueditor/define/State.java
@@ -0,0 +1,17 @@
+package com.jeesite.common.ueditor.define;
+
+/**
+ * 处理状态接口
+ * @author hancong03@baidu.com
+ */
+public interface State {
+	
+	boolean isSuccess();
+	
+	void putInfo(String name, String val);
+	
+	void putInfo(String name, long val);
+	
+	String toJSONString();
+
+}
---
+++ web/src/main/java/com/jeesite/modules/Application.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2013-Now http://jeesite.com All rights reserved.
+ * No deletion without permission, or be held responsible to law.
+ */
+package com.jeesite.modules;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Application
+ * @author ThinkGem
+ * @version 2018-10-13
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		logger.info("项目启动成功");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		this.setRegisterErrorPageFilter(false); // 错误页面有容器来处理，而不是SpringBoot
+		return builder.sources(Application.class);
+	}
+
+}
